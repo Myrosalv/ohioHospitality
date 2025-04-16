ca0a1eeab710f64f70c27fb9a04bcc3cb703bfef
Myrosalv <myroslav.kogut20007@gmail.com>
2025-04-16 14:33:13 -0400

    Initial Commit

---
+++ ohioHospitality/ohioHospitalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ohioHospitalityApp.swift
+//  ohioHospitality
+//
+//  Created by Myroslav Kohut on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ohioHospitalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ohioHospitalityTests/ohioHospitalityTests.swift
@@ -0,0 +1,17 @@
+//
+//  ohioHospitalityTests.swift
+//  ohioHospitalityTests
+//
+//  Created by Myroslav Kohut on 4/16/25.
+//
+
+import Testing
+@testable import ohioHospitality
+
+struct ohioHospitalityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
